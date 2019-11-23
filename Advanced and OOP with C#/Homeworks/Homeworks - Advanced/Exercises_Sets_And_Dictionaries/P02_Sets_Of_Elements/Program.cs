40c8dd99a7843d76e44e847ad93ca490196595e5
SharkovIT <47569303+SharkovIT@users.noreply.github.com>
2019-11-23 14:28:40 +0200

    Upload Homework

---
+++ Advanced and OOP with C#/Homeworks/Homeworks - Advanced/Exercises_Sets_And_Dictionaries/P02_Sets_Of_Elements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_Sets_Of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < numbers[0]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                firstSet.Add(number);
+            }
+            for (int i = 0; i < numbers[1]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                secondSet.Add(number);
+            }
+
+            foreach (var item in firstSet)
+            {
+                if (secondSet.Contains(item))
+                {
+                    Console.Write($"{item} ");
+                }
+            }
+        }
+    }
+}
---
+++ Advanced and OOP with C#/Homeworks/Homeworks - Advanced/Exercises_Sets_And_Dictionaries/P03_Periodic_Table/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            SortedSet<string> chemicalElements = new SortedSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var input = Console.ReadLine()
+                     .Split()
+                     .ToList();
+
+                for (int j = 0; j < input.Count; j++)
+                {
+                    chemicalElements.Add(input[j]);
+                }
+            }
+
+            foreach (var element in chemicalElements)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+    }
+}
