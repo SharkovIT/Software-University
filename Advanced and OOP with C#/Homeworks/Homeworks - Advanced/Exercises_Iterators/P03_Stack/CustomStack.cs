d88f10ff0e4f5a6d8b5efd62f9d5c85029241f06
SharkovIT <47569303+SharkovIT@users.noreply.github.com>
2019-11-24 19:50:23 +0200

    All Problems solved

---
+++ Advanced and OOP with C#/Homeworks/Homeworks - Advanced/Exercises_Iterators/P03_Stack/CustomStack.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+
+        private Stack<T> data;
+
+        public CustomStack()
+        {
+            this.data = new Stack<T>();
+        }
+
+        public void Pop()
+        {
+            if (this.data.Count > 0)
+            {
+                data.Pop();
+            }
+            else
+            {
+                Console.WriteLine("No elements");
+            }
+        }
+
+        public void Push(T value)
+        {
+            this.data.Push(value);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in this.data)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ Advanced and OOP with C#/Homeworks/Homeworks - Advanced/Exercises_Iterators/P03_Stack/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace P03_Stack
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var data = new CustomStack<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+
+                if (input.Contains("Push"))
+                {
+                    var elements = input.Split(new string[] { ", ", " " }, StringSplitOptions.RemoveEmptyEntries).Skip(1);
+
+                    foreach (var item in elements)
+                    {
+                        data.Push(item);
+                    }
+                }
+                else
+                {
+                    data.Pop();
+                }
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                foreach (var value in data)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+        }
+    }
+}
