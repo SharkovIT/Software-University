11570ce31352e3a5fa290236edafc9afdda41072
SharkovIT <47569303+SharkovIT@users.noreply.github.com>
2019-11-24 16:04:03 +0200

    Upload Homework

---
+++ Advanced and OOP with C#/Homeworks/Homeworks - Advanced/Exercises_Iterators/Comparing_Objects/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Comparing_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Person> people = new List<Person>();
+
+            while (input != "END")
+            {
+                string[] splittedInput = input.Split();
+
+                string name = splittedInput[0];
+                int age = int.Parse(splittedInput[1]);
+                string town = splittedInput[2];
+
+                Person person = new Person(name, age, town);
+                people.Add(person);
+
+                input = Console.ReadLine();
+            }
+
+            int n = int.Parse(Console.ReadLine());
+            Person targedPerson = null;
+
+            int matchPeople = 0;
+            int dismatchPeople = 0;
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                if (n - 1 == i)
+                {
+                    targedPerson = people[i];
+                }
+            }
+
+            foreach (var currentPerson in people)
+            {
+                if (currentPerson.CompareTo(targedPerson) == 0)
+                {
+                    matchPeople++;
+                }
+                else
+                {
+                    dismatchPeople++;
+                }
+            }
+
+            if (matchPeople == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{matchPeople} {dismatchPeople} {people.Count}");
+            }
+        }
+    }
+}
---
+++ Advanced and OOP with C#/Homeworks/Homeworks - Advanced/Exercises_Iterators/P01_ListyIterator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_ListyIterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> listy = null;
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                try
+                {
+                    if (command.Contains("Create"))
+                    {
+                        List<string> items = command.Split().Skip(1).ToList();
+                        listy = new ListyIterator<string>(items);
+                    }
+                    else if (command == "Print")
+                    {
+                        listy.Print();
+                    }
+                    else if (command == "HasNext")
+                    {
+                        Console.WriteLine(listy.HasNext());
+                    }
+                    else if (command == "Move")
+                    {
+                        Console.WriteLine(listy.Move());
+                    }
+                    else if (command == "PrintAll")
+                    {
+                        foreach (var item in listy)
+                        {
+                            Console.Write(item + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+
+                command = Console.ReadLine();
+            }
+
+        }
+    }
+}
