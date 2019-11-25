a6b1f0d582f5b859dd170ace0a0349eba26b08c0
SharkovIT <47569303+SharkovIT@users.noreply.github.com>
2019-11-25 18:00:26 +0200

    First task done

---
+++ Entity Framework/Homeworks/XML_Processing_Exercise/CarDealer/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.Dtos.Import;
+using CarDealer.Models;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Mapper.Initialize(cfg => cfg.AddProfile<CarDealerProfile>());
+
+            using (var db = new CarDealerContext())
+            {
+                var inputXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+
+                var result = ImportSuppliers(db, inputXml);
+
+                System.Console.WriteLine(result);
+            }
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(ImportSupplierDto[]),
+                                new XmlRootAttribute("Suppliers"));
+
+            var suppliersDto = (ImportSupplierDto[])xmlSerializer.Deserialize(new StringReader(inputXml));
+
+            var suppliers = Mapper.Map<Supplier[]>(suppliersDto);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Length}";
+        }
+    }
+}
