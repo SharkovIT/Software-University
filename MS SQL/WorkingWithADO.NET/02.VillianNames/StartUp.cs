9ffc10a188e235e644d321b8a693572d5e82a5dc
SharkovIT <47569303+SharkovIT@users.noreply.github.com>
2019-11-29 14:57:01 +0200

    All Problems Solved

---
+++ MS SQL/WorkingWithADO.NET/02.VillianNames/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using _01.InitialSetup;
+using System;
+using System.Data.SqlClient;
+
+namespace _02.VillianNames
+{
+    public class StartUp
+    {
+        private const string DbName = "MinionsDB";
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(String.Format(Configuration.ConnectionString, DbName));
+
+            connection.Open();
+
+            using (connection)
+            {
+                 SqlCommand sqlCommand = new SqlCommand(Queries.VillainNames, connection);
+
+                try
+                {
+                     SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        string villainName = (string)sqlDataReader["Name"];
+                        int minionsCount = (int)sqlDataReader["MinionsCount"];
+
+                        Console.WriteLine($"{villainName} - {minionsCount}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Invalid operation: {ex.Message}");
+                }
+            }
+        }
+    }
+}
