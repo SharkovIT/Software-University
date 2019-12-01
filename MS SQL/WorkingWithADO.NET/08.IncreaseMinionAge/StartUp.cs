f49108d6cbe401d40250ecb43e155df09f8be4d8
SharkovIT <47569303+SharkovIT@users.noreply.github.com>
2019-12-01 23:25:04 +0200

    Upload Homework

---
+++ MS SQL/WorkingWithADO.NET/08.IncreaseMinionAge/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using _01.InitialSetup;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace _08.IncreaseMinionAge
+{
+    public class StartUp
+    {
+        private const string DbName = "MinionsDB";
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(String.Format(Configuration.ConnectionString, DbName));
+
+            connection.Open();
+
+            using (connection)
+            {
+                List<int> minionsIds = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+                SqlCommand updateCmd = new SqlCommand(Queries.UpdateMinions, connection);
+
+                for (int i = 0; i < minionsIds.Count; i++)
+                {
+                    int currentId = minionsIds[i];
+                    updateCmd.Parameters.AddWithValue("@Id", currentId);
+                    updateCmd.ExecuteNonQuery();
+                    updateCmd.Parameters.Clear();
+                }
+
+                SqlCommand resultCmd = new SqlCommand(Queries.TakeMinionsNameAndAge, connection);
+                SqlDataReader reader = resultCmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string minionName = (string)reader["Name"];
+                    int minionAge = (int)reader["Age"];
+
+                    Console.WriteLine($"{minionName} {minionAge}");
+                }
+            }
+        }
+    }
+}
---
+++ MS SQL/WorkingWithADO.NET/09.IncreaseAgeStoredProcedure/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using _01.InitialSetup;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _09.IncreaseAgeStoredProcedure
+{
+    public class StartUp
+    {
+        private const string DbName = "MinionsDB";
+        private const string ProcedureName = "usp_GetOlder";
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection(string.Format(Configuration.ConnectionString, DbName));
+
+            connection.Open();
+
+            using (connection)
+            {
+                int minionId = int.Parse(Console.ReadLine());
+
+                // Create procedure
+                SqlCommand createCommand = new SqlCommand(Queries.CreateProcedure, connection);
+                createCommand.ExecuteNonQuery();
+
+                // Exec procedure
+                SqlCommand execCommand = new SqlCommand(ProcedureName, connection);
+
+                execCommand.CommandType = CommandType.StoredProcedure;
+                execCommand.Parameters.AddWithValue("@Id", minionId);
+                execCommand.ExecuteNonQuery();
+
+                // Create reader to show result after procedure executes
+                SqlCommand selectCommand = new SqlCommand(Queries.SelectMinionNameAndAge, connection);
+                selectCommand.Parameters.AddWithValue("@Id", minionId);
+
+                SqlDataReader reader = selectCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    string minionName = (string)reader["Name"];
+                    int minionAge = (int)reader["Age"];
+
+                    Console.WriteLine($"{minionName} - {minionAge} years old");
+                }
+            }
+        }
+    }
+}
