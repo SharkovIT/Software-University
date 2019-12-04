2a341dc13a1fdee4c31df122aace97c532b59191
SharkovIT <47569303+SharkovIT@users.noreply.github.com>
2019-12-04 16:05:43 +0200

    Last Task Left

---
+++ Entity Framework/Exams/C# DB Advanced Retake Exam - 01.09.2018/VaporStore/DataProcessor/ExportDtos/GenreExportDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaporStore.DataProcessor.ExportDtos
+{
+    public class GenreExportDto
+    {
+        public int Id { get; set; }
+
+        public string Genre { get; set; }
+
+        public GameExportDto[] Games { get; set; }
+
+        public int TotalPlayers { get; set; }
+    }
+}
--- Entity Framework/Exams/C# DB Advanced Retake Exam - 01.09.2018/VaporStore/DataProcessor/Serializer.cs
+++ Entity Framework/Exams/C# DB Advanced Retake Exam - 01.09.2018/VaporStore/DataProcessor/Serializer.cs
@@ -2,17 +2,46 @@
 {
 	using System;
     using System.IO;
+    using System.Linq;
     using System.Text;
     using System.Xml;
     using System.Xml.Serialization;
     using Data;
+    using Newtonsoft.Json;
+    using VaporStore.DataProcessor.ExportDtos;
 
-	public static class Serializer
+    public static class Serializer
 	{
 		public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
 		{
-			throw new NotImplementedException();
-		}
+            var result = context.Genres
+             .Where(g => genreNames.Contains(g.Name))
+             .Select(genre => new GenreExportDto
+             {
+                 Id = genre.Id,
+                 Genre = genre.Name,
+                 Games = genre.Games
+                     .Where(g => g.Purchases.Any())
+                     .Select(game => new GameExportDto
+                     {
+                         Id = game.Id,
+                         Title = game.Name,
+                         Developer = game.Developer.Name,
+                         Tags = string.Join(", ", game.GameTags.Select(g => g.Tag.Name)),
+                         Players = game.Purchases.Count
+                     })
+                     .OrderByDescending(game => game.Players)
+                     .ThenBy(game => game.Id)
+                     .ToArray(),
+                 TotalPlayers = genre.Games.Sum(g => g.Purchases.Count)
+             })
+             .OrderByDescending(g => g.TotalPlayers)
+             .ThenBy(g => g.Id)
+             .ToArray();
+
+            var json = JsonConvert.SerializeObject(result, Newtonsoft.Json.Formatting.Indented);
+            return json;
+        }
 
 		public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
 		{
