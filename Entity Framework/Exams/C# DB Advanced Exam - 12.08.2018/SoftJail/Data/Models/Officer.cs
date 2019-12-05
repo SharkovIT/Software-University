729c2c9c037e63a52595534355dfc90fea6b778b
SharkovIT <47569303+SharkovIT@users.noreply.github.com>
2019-12-05 16:13:51 +0200

    First Task Solved

---
+++ Entity Framework/Exams/C# DB Advanced Exam - 12.08.2018/SoftJail/Data/Models/Officer.cs
@@ -0,0 +1,35 @@
+﻿using SoftJail.Data.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SoftJail.Data.Models
+{
+    public class Officer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MinLength(3), MaxLength(30)]
+        public string FullName { get; set; }
+
+        [Required, Range(0.0, double.MaxValue)]
+        public decimal Salary { get; set; }
+
+        [Required]
+        public Position Position { get; set; }
+
+        [Required]
+        public Weapon Weapon { get; set; }
+
+        public int? DepartmentId { get; set; }
+        
+        [Required]
+        public Department Department { get; set; }
+
+        public ICollection<OfficerPrisoner> OfficerPrisoners { get; set; }
+                        = new HashSet<OfficerPrisoner>();
+    }
+}
