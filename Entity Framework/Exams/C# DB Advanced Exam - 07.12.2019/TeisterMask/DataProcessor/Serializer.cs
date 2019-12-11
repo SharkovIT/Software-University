be1c50fab7cb01c69e27070d4b67bb400593a3b3
SharkovIT <47569303+SharkovIT@users.noreply.github.com>
2019-12-11 14:39:09 +0200

    Last Task Left

--- Entity Framework/Exams/C# DB Advanced Exam - 07.12.2019/TeisterMask/DataProcessor/Serializer.cs
+++ Entity Framework/Exams/C# DB Advanced Exam - 07.12.2019/TeisterMask/DataProcessor/Serializer.cs
@@ -9,15 +9,40 @@
 
     public class Serializer
     {
-        public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
-        {
-            throw new NotImplementedException();
-        }
 
         public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            var employees = context.Employees
+              .Where(e => e.EmployeesTasks
+                  .Any(et => et.Task.OpenDate.Date >= date))
+              .Select(e => new 
+              {
+                  Username = e.Username,
+                  Tasks = e.EmployeesTasks
+                      .Select(et => new 
+                      {
+                          TaskName = et.Task.Name,
+                          OpenDate = et.Task.OpenDate.ToString("d", CultureInfo.InvariantCulture),
+                          DueDate = et.Task.DueDate.ToString("d", CultureInfo.InvariantCulture),
+                          LabelType = et.Task.LabelType.ToString(),
+                          ExecutionType = et.Task.ExecutionType.ToString()
+                      })
+                      .OrderByDescending(et => et.DueDate)
+                      .ThenBy(et => et.TaskName)
+                      .ToArray()
+              })
+              .ToArray()
+              .OrderByDescending(e => e.Tasks.Length)
+              .ThenBy(e => e.Username)
+              .Take(10)
+              .ToArray();
 
+
+            return JsonConvert.SerializeObject(employees, Formatting.Indented);
+        }
+        public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
+        {
+            throw new NotImplementedException();
         }
     }
 }
